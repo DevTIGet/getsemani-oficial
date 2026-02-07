f4ee044890d5064fa8064852c9ccf0a81b625ec4
Welbert-Soares <welbertsoares6@gmail.com>
2026-02-07 17:05:36 -0300

    config - organizando arquitetura.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import EmblaCarousel from "@/app/components/EmblaCarousel";
 import WelcomeSection from "@/app/components/WelcomeSection";
 import TVGetSection from "@/app/components/TVGetSection";
-import UpcomingEventsSection from "@/app/components/UpcomingEventsSection";
-import ContributionSection from "@/app/components/ContributionSection";
+import UpcomingEventsSection from "@/app/components/Upcoming/UpcomingEventsSection";
+import ContributionSection from "@/app/components/Contribution/ContributionSection";
 import { CAROUSEL_SLIDES, CAROUSEL_OPTIONS } from "@/app/constants/carousel";
 
 export default function Home() {
